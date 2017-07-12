7125d132a9054718cc5ee2f9b18c19c8ff948ba9
IvanOpushkin <farm.vanya@yandex.ru>
2017-07-12 10:11:43 +1000

    4th commit full plan of working app

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -32,7 +33,63 @@ public class UserMealsUtil {
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
        // System.out.println("TODO return filtered list with correctly exceeded field");
-        Stream<UserMeal> meals = mealList.stream();
+
+
+        List<UserMealWithExceed> finalList = new ArrayList<UserMealWithExceed>();
+
+
+        //Проверка даты
+        for (UserMeal userMeal : mealList)
+        {
+            //Простое заполнение по параметрам это легко, создавая новый элемент new UserMealWithExceed();
+
+            //Нужна второстепенная мапа в <ЛокалДэйт,Количество каллорий всего дня> затем перебор после сравнения по ключу даты get(key) returns
+            //каллорис дня, и фильтрануть их со временем и каллорийностью одновременно. (Прям в фильтре сравнивать по датам из готовой мапы каллории))
+            //Как бы поэффективней это делать. Наверно 1.Мапа с фул каллориями. 2.Фильтр списка по времени.
+            // 3.Перенос списка в список UserMealWithExceed (уже подходящего по времени из п2) с добавлением по мапе только Exceed значений тру.
+
+            //Нука оценим тайм комплексити O(n2) - бабл сорт или инсерт сорт. Перебор двумерный.
+            //O(n) перебор одномерный. типо наибольшего числа в массиве. пробросом линейно 1 раз.
+
+            //Итого первая мапа перебор линейный O(n) первый пункт
+            //Второй пункт время перебирается линейно и каллорийность чекается линейно (по идее сначала же проверяется первое условие в && (И)
+            //до второго дойдёт только если время подошло. Вторая игра с мапами тоже линейная O(n)
+            //Третье перенос отфильтрованых значений в лист тоже O(n)
+
+
+            //Таинство это очень круто. В нужный момент, когда краски раскрылись и фантазии читателя наполнилась, дать ему поиграть со своей фантазией.
+            //облегчить сцену как вариант, атмосферу возможно оставить.
+        }
+
+        Stream<UserMeal> meals = mealList.stream().filter(p -> p.getDateTime().toLocalTime().toNanoOfDay() > startTime.toNanoOfDay()
+                && p.getDateTime().toLocalTime().toNanoOfDay() < endTime.toNanoOfDay());
+
+
+        meals = meals.filter(p -> p.getCalories() > 999);
+
+
+        //1.Групировка по времени, наверно сделать второй, но ещё заранее сделать полный список по дням
+        //Тоесть план
+        //Первое по дням
+        //Второе время + чек параметра exceed
+
+        //Stream<UserMeal> meals = mealList.stream().filter(p -> p.getDateTime().toLocalTime().toNanoOfDay() > startTime.toNanoOfDay()
+                //&& p.getDateTime().toLocalTime().toNanoOfDay() < endTime.toNanoOfDay());
+
+
+
+        long a = meals.count();
+        System.out.println(a);
+
+
+        meals.close();
+
+
+
+
+
+
+
 
         return null;
     }
